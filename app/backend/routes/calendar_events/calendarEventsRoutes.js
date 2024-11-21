68f6056ec7521896f642b9d1d5278660c9d78b81
Josh Cox <117310223+The-Average-Coder@users.noreply.github.com>
2024-11-21 13:23:32 +0000

    Fixed Calendar to show next year's events
    Previously code fetched events from the current and previous year, but not the next year, so this has been fixed.

--- app/backend/routes/calendar_events/calendarEventsRoutes.js
+++ app/backend/routes/calendar_events/calendarEventsRoutes.js
@@ -4,7 +4,7 @@ var db = require('../../db');
 const router = express.Router();
 
 router.get('/getRecentCalendarEvents', (req, res) => {
-    const sqlSelect = 'SELECT calendar_events.*, repairers.color, repairers.id AS repairer_id, repairs.status, instruments.type FROM calendar_events INNER JOIN repairs ON repairs.id = calendar_events.repair_id LEFT JOIN repairers ON repairers.id = repairs.repairer_id LEFT JOIN instruments ON instruments.id = repairs.instrument_id WHERE EXTRACT(year from calendar_events.start) = EXTRACT(year from CURRENT_TIMESTAMP) or EXTRACT(year from calendar_events.start) = EXTRACT(year from CURRENT_TIMESTAMP) - 1;';
+    const sqlSelect = 'SELECT calendar_events.*, repairers.color, repairers.id AS repairer_id, repairs.status, instruments.type FROM calendar_events INNER JOIN repairs ON repairs.id = calendar_events.repair_id LEFT JOIN repairers ON repairers.id = repairs.repairer_id LEFT JOIN instruments ON instruments.id = repairs.instrument_id WHERE EXTRACT(year from calendar_events.start) = EXTRACT(year from CURRENT_TIMESTAMP) or EXTRACT(year from calendar_events.start) = EXTRACT(year from CURRENT_TIMESTAMP) - 1 or EXTRACT(year from calendar_events.start) = EXTRACT(year from CURRENT_TIMESTAMP) + 1;';
 
     db.query(sqlSelect, (err, result) => {
         if (err) {
